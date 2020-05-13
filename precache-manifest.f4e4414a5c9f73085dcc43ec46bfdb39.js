e621d7f8eab75a13d29bc5ae0d3edba479f471ce
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-05-13 11:34:27 +0300

    Updates

---
+++ precache-manifest.f4e4414a5c9f73085dcc43ec46bfdb39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de7809308482ef8c56308a87bbe4e9b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e7eb751df9ae22a554fa",
+    "url": "/monster-rolodex/static/css/main.251c7a95.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7eb751df9ae22a554fa",
+    "url": "/monster-rolodex/static/js/main.adfe32dc.chunk.js"
+  },
+  {
+    "revision": "fdce5faec76031078f23",
+    "url": "/monster-rolodex/static/js/runtime-main.c9e8ecbd.js"
+  }
+]);
